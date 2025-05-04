decc8fd01ca889cebc6aa56eb91e66c3cfd8ba61
Thomas <tjgriffin77@gmail.com>
2025-05-04 16:30:46 -0400

    Changes - added sitemap - updated robots.txt - changed projects to portfolio - implemented accordian into portfolio

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,14 @@
+import PortfolioAccordian from "../components/PortfolioAccordian";
+
+function Portfolio() {
+  return (
+    <div>
+      <div className="h-screen max-w-screen-xl mx-auto">
+        <h1 className="text-3xl font-bold mx-4 my-8">Portfolio</h1>
+        <PortfolioAccordian />
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
